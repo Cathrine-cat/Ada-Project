3eeaaaa2acd275cf67553c7cb9ae9c23ef8af60b
Cristina <cristinacontiunicole@gmail.com>
2025-04-23 17:55:59 +0300

    new commit

--- src/InputBitStream.java
+++ src/InputBitStream.java
@@ -15,10 +15,26 @@ public class InputBitStream implements Cloneable{
     public boolean readBit() throws IOException {
         if(numBitsRemaining == 0) {
             currByte = input.read();
-            if(currByte == -1) throw  new EOFException();
+            if(currByte == -1) throw new EOFException();
             numBitsRemaining=8;
         }
         numBitsRemaining--;
         return ((currByte >> numBitsRemaining) & 1) ==1;
+    }
+
+    public int readByte() throws IOException {
+        int result = 0;
+        for (int i = 0; i < 8; i++) {
+            result = (result << 1) | (readBit() ? 1 : 0);
+        }
+        return result;
+    }
+
+    public int readInt() throws IOException {
+        return (readByte()<<24) | (readByte()<<16) | (readByte()<<8) | readByte();
+    }
+
+    public void close() throws IOException {
+        input.close();
     }
 }
